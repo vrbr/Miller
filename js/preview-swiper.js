7c4d793bb422aae2e0a731551dfbadb3f1501b17
Мария <lpvamaria@gmail.com>
2021-10-25 22:34:24 +0300

    Планшет и лаптоп; отключен слайдер на лаптопе

--- js/preview-swiper.js
+++ js/preview-swiper.js
@@ -1,17 +1,23 @@
-const previewSwiper = new Swiper('.preview-swiper', {
-    effect: "coverflow",
-    grabCursor: true,
-    centeredSlides: true,
-    slidesPerView: "auto",
-    coverflowEffect: {
-        scale: 0.8,
-        stretch: 0,
-        depth: 100,
-        modifier: 0.7,
-        slideShadows: false,
-    },
-    pagination: {
-        el: ".swiper-pagination.preview-pagination",
-        clickable: true,
-    },
+document.addEventListener('DOMContentLoaded', () => {
+    const width = window.innerWidth
+    if (width < 1349){
+        const previewSwiper = new Swiper('.preview-swiper', {
+            effect: "coverflow",
+            grabCursor: true,
+            centeredSlides: true,
+            slidesPerView: "auto",
+            initialSlide: 1,
+            coverflowEffect: {
+                scale: 0.8,
+                stretch: 0,
+                depth: 100,
+                modifier: 0.7,
+                slideShadows: false,
+            },
+            pagination: {
+                el: ".swiper-pagination.preview-pagination",
+                clickable: true,
+            },
+        });
+    }
 });
