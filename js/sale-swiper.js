9df9899737738e77641fbb2800684551b2eb5a39
Мария <lpvamaria@gmail.com>
2021-10-26 09:03:08 +0300

    Блок sale на мобиле; новый свайпер

---
+++ js/sale-swiper.js
@@ -0,0 +1,8 @@
+const swiper = new Swiper(".mySwiper.sale-swiper", {
+    slidesPerView: "auto",
+    spaceBetween: 20,
+    navigation: {
+        nextEl: ".swiper-sale-button-next",
+        prevEl: ".swiper-sale-button-prev",
+    },
+});
