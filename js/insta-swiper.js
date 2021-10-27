237846c51ede72eefe5fc214a670ee22e3d3834d
Мария <lpvamaria@gmail.com>
2021-10-27 19:52:15 +0300

    Блок на планшете

--- js/insta-swiper.js
+++ js/insta-swiper.js
@@ -1,16 +1,4 @@
-// var swiper = new Swiper(".mySwiper", {
-//     slidesPerView: 3,
-//     grid: {
-//         rows: 2,
-//     },
-//     spaceBetween: 30,
-//         pagination: {
-//         el: ".swiper-pagination",
-//         clickable: true,
-//     },
-// });
-var instaSwiper = new Swiper(".insta__swiper", {
-    // slidesPerView: 2,
+const instaSwiper = new Swiper(".insta__swiper", {
     slidesPerView: "auto",
     spaceBetween: 20,
     navigation: {
