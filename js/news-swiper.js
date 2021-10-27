4802f30ff39c23ab08b6ffa8cf3dc4de67001e79
Мария <lpvamaria@gmail.com>
2021-10-27 11:58:20 +0300

    Блок news для мобилы

---
+++ js/news-swiper.js
@@ -0,0 +1,8 @@
+const newsSwiper = new Swiper(".news-swiper", {
+    slidesPerView: "auto",
+    spaceBetween: 20,
+    navigation: {
+        nextEl: ".news-swiper-button-next",
+        prevEl: ".news-swiper-button-prev",
+    },
+});
