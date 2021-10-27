4209f93d9122fa29f9605b2f4369f006f858b880
Мария <lpvamaria@gmail.com>
2021-10-27 13:55:19 +0300

    Блок на лаптопе; отключен свайпер

--- js/news-swiper.js
+++ js/news-swiper.js
@@ -1,13 +1,18 @@
-const newsSwiper = new Swiper(".news-swiper", {
-    slidesPerView: "auto",
-    spaceBetween: 20,
-    navigation: {
-        nextEl: ".news-swiper-button-next",
-        prevEl: ".news-swiper-button-prev",
-    },
-    breakpoints: {
-        768: {
-            spaceBetween: 40,
-        },
-    },
+document.addEventListener('DOMContentLoaded', () => {
+    const width = window.innerWidth
+    if (width < 1349){
+        const newsSwiper = new Swiper(".news-swiper", {
+            slidesPerView: "auto",
+            spaceBetween: 20,
+            navigation: {
+                nextEl: ".news-swiper-button-next",
+                prevEl: ".news-swiper-button-prev",
+            },
+            breakpoints: {
+                768: {
+                    spaceBetween: 40,
+                },
+            },
+        });
+    }
 });
